8833e7d0e5c00c1c44273aefb7225c55fa8bf8de
dushyantbhati2 <db90054@gmail.com>
2024-06-29 20:39:44 +0530

    all bugs fixed on register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -22,7 +22,7 @@ const registerUser=asyncHandler( async (req,res) =>{
         throw new ApiError(400,"All fields are required")
     }
 
-    const existedUser=User.findOne({
+    const existedUser= await User.findOne({
         $or:[{username},{email}]
     })
 
@@ -31,8 +31,12 @@ const registerUser=asyncHandler( async (req,res) =>{
     }
 
     const avatarLocalPath=req.files?.avatar[0]?.path
-    const coverImageLocalPath=req.files?.coverImage[0]?.path
+    // const coverImageLocalPath=req.files?.coverImage[0]?.path
 
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar is required")
     }
@@ -52,12 +56,13 @@ const registerUser=asyncHandler( async (req,res) =>{
 
     })
 
-    const createdUser=User.findById(user._id).select(
+    const createdUser=await User.findById(user._id).select(
         "-password -refreshToken"
     )
     if(!createdUser){
         throw new ApiError(500,"Something went wrong while registering the user")
     }
+    console.log(createdUser)
     return res.status(201).json(
         new ApiResponse(200,createdUser,"User Sucessfully Registered")
     )
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -13,7 +13,8 @@ const uploadOnCloudinary=async (localFilePath)=>{
         const response=await cloudinary.uploader.upload(localFilePath,{
             resource_type:"auto"
         })
-        console.log("File successfully uploaded",response.url)
+        // console.log("File successfully uploaded",response.url)
+        fs.unlinkSync(localFilePath)
         return response
     } catch (error) {
         fs.unlinkSync(localFilePath)
